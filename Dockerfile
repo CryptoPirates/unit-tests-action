ee00697d0f6dbba0c7399af1dbbe5c42a1cd3223
Colin Campbell <colinc86@gmail.com>
2019-10-02 00:49:35 -0500

    Initial commit.

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM golang:1.13-alpine as builder
+
+COPY entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+apk --update --upgrade add git
+
+export GOPRIVATE=github.com/cryptopirates
+
+echo "Configuring git"
+git config --global url."https://${INPUT_USERNAME}:${INPUT_ACCESSTOKEN}@github.com".insteadOf "https://github.com"
+
+if [ "${INPUT_REQUIRESTALIB}" == "true" ]; then
+    echo "Installing TA-Lib"
+    git clone https://github.com/cryptopirates/ta-lib.git
+    cd ta-lib
+    git checkout gh-actions-alpine-latest
+    sudo make install
+fi
+
+echo "Cloning into ${GITHUB_REPOSITORY}"
+git clone "git@github.com:cryptopirates/${INPUT_REPOSITORYNAME}.git"
+cd "${INPUT_REPOSITORYNAME}"
+
+echo "Getting dependencies"
+go get -v -t -d ./...
+
+echo "Running tests"
+go test -v
