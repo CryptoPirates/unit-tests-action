930850905b0e29df515b28e87522eefc6cdcb916
Colin Campbell <colinc86@gmail.com>
2019-10-02 23:53:21 -0500

    Try it.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,18 @@
 
 export GOPRIVATE=github.com/cryptopirates
 
-echo "Configuring git ${TESTUSER}"
+echo "Configuring git"
+echo "argument 1" $0
+echo "argument 2" $1
 git config --global url."https://${INPUT_USERNAME}:${INPUT_ACCESSTOKEN}@github.com".insteadOf "https://github.com"
 
 if [ $INPUT_REQUIRESTALIB = "true" ]; then
     echo "Installing TA-Lib"
     git clone https://github.com/cryptopirates/ta-lib.git
     cd ta-lib
-    git checkout gh-actions-alpine-latest
+    # git checkout gh-actions-alpine-latest
+    ./configure --prefix=/usr
+    make
     make install
 fi
 
