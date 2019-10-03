d1b47b08cb7b5f678cf7cd8e0096b2d9d9c0131d
Colin Campbell <colinc86@gmail.com>
2019-10-03 00:33:01 -0500

    Update ta-lib binaries.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@
 export GOPRIVATE=github.com/cryptopirates
 
 echo "Configuring git"
+git config --global user.email "colinc86@gmail.com"
+git config --global user.name "Colin Campbell"
 git config --global url."https://${INPUT_USERNAME}:${INPUT_ACCESSTOKEN}@github.com".insteadOf "https://github.com"
 
 if [ $INPUT_REQUIRESTALIB = "true" ]; then
@@ -10,7 +12,12 @@ if [ $INPUT_REQUIRESTALIB = "true" ]; then
     git clone https://github.com/cryptopirates/ta-lib.git
     cd ta-lib
     git checkout gh-actions-buster-latest
-    make install
+    ./configure-lm --prefix=/usr
+    make
+    git add .
+    git commit -m "Use math libraries."
+    git push
+    # make install
 fi
 
 git clone "https://github.com/cryptopirates/${INPUT_REPOSITORYNAME}.git"
