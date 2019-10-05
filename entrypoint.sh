b855df9c41f1d434ad62fb45479f49cc9cadc9a9
Colin Campbell <colinc86@gmail.com>
2019-10-04 22:11:32 -0500

    Update bash.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,21 +3,14 @@
 export GOPRIVATE=github.com/cryptopirates
 
 echo "Configuring git"
-git config --global user.email "colinc86@gmail.com"
-git config --global user.name "Colin Campbell"
 git config --global url."https://${INPUT_GITUSERNAME}:${INPUT_GITACCESSTOKEN}@github.com".insteadOf "https://github.com"
 
 if [ "${INPUT_REQUIRESTALIB}" == "true" ]; then
     echo "Installing TA-Lib"
     git clone https://github.com/cryptopirates/ta-lib.git
     cd ta-lib
-    git checkout -b gh-actions-alpine2-latest
-    ./configure
-    make
-    git add .
-    git commit -m "Update build binaries."
-    git push --set-upstream origin gh-actions-alpine2-latest
-    # make install
+    git checkout gh-actions-alpine2-latest
+    make install
     cd ..
 fi
 
