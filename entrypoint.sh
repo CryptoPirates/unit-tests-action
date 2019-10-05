462bd30fdce2071afa1fa392a4e3489310baf85b
Colin Campbell <colinc86@gmail.com>
2019-10-04 22:06:21 -0500

    Update ta-lib binaries.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@
 export GOPRIVATE=github.com/cryptopirates
 
 echo "Configuring git"
+git config --global user.email "colinc86@gmail.com"
+git config --global user.name "Colin Campbell"
 git config --global url."https://${INPUT_GITUSERNAME}:${INPUT_GITACCESSTOKEN}@github.com".insteadOf "https://github.com"
 
 if [ "${INPUT_REQUIRESTALIB}" == "true" ]; then
@@ -10,7 +12,13 @@ if [ "${INPUT_REQUIRESTALIB}" == "true" ]; then
     git clone https://github.com/cryptopirates/ta-lib.git
     cd ta-lib
     git checkout gh-actions-alpine-latest
-    make install
+    ./configure
+    make
+    git add .
+    git commit -m "Update build binaries."
+    git push
+    # make install
+    cd ..
 fi
 
 git clone "https://github.com/${GITHUB_REPOSITORY}.git"
