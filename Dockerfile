e2847b5f797835c711fa95218b4b54c72ba46157
Colin Campbell <colinc86@gmail.com>
2020-05-04 23:53:34 -0500

    Use bpc base image.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.13-alpine
+FROM docker.pkg.github.com/cryptopirates/bpc-base-image/bpc-base:0.1.0
 
 RUN apk add --update bash git build-base coreutils
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,25 +5,6 @@ export GOPRIVATE=github.com/cryptopirates
 echo "Configuring git"
 git config --global url."https://${INPUT_GITUSERNAME}:${INPUT_GITACCESSTOKEN}@github.com".insteadOf "https://github.com"
 
-if [ "${INPUT_REQUIRESTALIB}" == "true" ]; then
-    echo "Installing TA-Lib"
-    git clone https://github.com/cryptopirates/ta-lib.git
-    cd ta-lib
-    git checkout gh-actions-alpine2-latest
-    make install
-    cd ..
-fi
-
-if [ "${INPUT_REQUIRESLIBRDKAFKA}" == "true" ]; then
-    echo "Installing librdkafka"
-    git clone https://github.com/edenhill/librdkafka.git
-    cd librdkafka
-    ./configure --install-deps --prefix /usr
-    make
-    make install
-    cd ..
-fi
-
 git clone "https://github.com/${GITHUB_REPOSITORY}.git"
 
 IFS='/'
